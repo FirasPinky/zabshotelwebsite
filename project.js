9a96cb52b5ea0c87e2d4d8412e4b681b677ab520
FirasPinky <156492093+FirasPinky@users.noreply.github.com>
2024-01-13 11:55:36 +0800

    Add files via upload

---
+++ project.js
@@ -0,0 +1,14 @@
+
+
+        // Call initAll when the window loads
+        window.onload = initAll;
+            function handleFormSubmit() {
+            // Add your form validation logic here if needed
+             
+            // Display alert box
+            alert("You are successful booking. Thank you!");
+
+            // Return false to prevent the form from actually submitting
+            return false;
+ }
+		
